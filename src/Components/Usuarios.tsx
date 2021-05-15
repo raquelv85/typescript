c7a462f0fbab5f32f557459f81774c427e308dec
RaquelV <raquelv85@gmail.com>
2021-05-15 23:50:02 +0200

    create component Usuarios and install axios

---
+++ src/Components/Usuarios.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+export const Usuarios = () => {
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <>
+      <h3>Usuarios:</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nombre</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
+  );
+};
