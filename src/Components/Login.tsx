1a5f7ef4cb5c898800700213dcdc17b090f279b2
RaquelV <raquelv85@gmail.com>
2021-05-15 23:12:25 +0200

    create interface AuthState and add to ininitalState, and create useReducer

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,21 +1,65 @@
+import { useReducer, useEffect } from "react";
 
+interface AuthState {
+  validando: boolean;
+  token: string | null;
+  username: string;
+  nombre: string;
+}
+
+const initialState: AuthState = {
+  validando: true,
+  token: null,
+  username: "",
+  nombre: "",
+};
+
+type AuthAction = {
+  type: "logout";
+};
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case "logout":
+      return {
+        validando: false,
+        token: null,
+        nombre: "",
+        username: "",
+      };
+
+    default:
+      return state;
+  }
+};
 
 export const Login = () => {
+  const [{ validando }, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "logout" });
+    }, 1500);
+  }, []);
+
+  if (validando) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className="alert alert-info">Validando..</div>
+      </>
+    );
+  }
+
   return (
     <>
       <h3>Login</h3>
-      <div className="alert alert-info">
-        Validando..
-      </div>
-      <div className="alert alert-danger">
-        No autenticado
-      </div>
-      <div className="alert alert-success">
-        Autenticado
-      </div>
+
+      <div className="alert alert-danger">No autenticado</div>
+      <div className="alert alert-success">Autenticado</div>
 
       <button className="btn btn-primary">Login</button>
       <button className="btn btn-danger">Logout</button>
     </>
-  )
-}
+  );
+};
