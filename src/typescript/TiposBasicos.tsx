1f9182ee786fa19d3db87a12e8e50e103eb8ac1d
RaquelV <raquelv85@gmail.com>
2021-05-15 19:58:10 +0200

    add basic types to component TiposBasicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,19 @@
 
 export const TiposBasicos = () => {
+
+  const nombre: string | number = "Raquel";
+  const edad: number = 35;
+  const activo: boolean = true;
+
+  const poderes: string[] = ['Velocidad', 'Volar', 'Desaparecer'];
+
+
   return (
     <>
       <h3>Tipos básicos</h3>
+      {nombre}, {edad}
+      <br/>
+      {poderes.join(', ')}
     </>
   )
 }
